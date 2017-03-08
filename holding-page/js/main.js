610782659d58a9fabe41c7ce3b09521648cdf549
Dave Thomson <djthomson@gmail.com>
2017-03-08 18:53:26 +0000

    Add extra features

--- holding-page/js/main.js
+++ holding-page/js/main.js
@@ -1,92 +1,71 @@
-// var source = Rx.Observable
-//     .interval(10000 /* ms */)
-//     .timeInterval()
-//     .take(3)
-//     .startWith(0);
-//
-// var subscription = source.subscribe(
-//   function (x) {
-//       console.log(x);
-//       if (x === 0) {
-//         renderLoadingScreen()
-//       }
-//       $('#root').html('Next: ' + x.value)
-//   },
-//   function (err) {
-//       console.log('Error: ' + err);
-//   },
-//   function () {
-//       console.log('Completed');
-//   });
+// Run
+$(document).ready(function() {
+  animateLoader(renderNextEventPage)
+});
 
-renderLoadingScreen()
-
-function renderLoadingScreen() {
-  var rootElement = document.getElementById('root')
-  var dots = 0
-  var interval = 1000
-
-  renderTitle()
-  renderLoader()
-  // animateLoader()
-
-  function renderTitle() {
-    var show = true
-    var LOADING_MSG = 'Loading Awesomes'
-    var titleElement = document.createElement('h1')
-    titleElement.innerHTML = LOADING_MSG
-    rootElement.appendChild(titleElement)
-    // setInterval(function() {
-    //   show = !show
-    //   titleElement.className = show ? '' : 'hidden'
-    // }, 700)
-  }
-
-  function renderLoader() {
-    var width = 0
-    var loaderElement = document.createElement('div')
-    loaderElement.setAttribute('id', 'loader')
-    var blockElement = document.createElement('span')
-    blockElement.setAttribute('id', 'block')
-    loaderElement.appendChild(blockElement)
-    console.log(loaderElement, blockElement)
-    rootElement.appendChild(loaderElement)
-    setInterval(function() {
-      console.log('LOADER')
-      if (width <= 100) {
-        blockElement.style.width = width + '%'
-        width += 10
-        console.log('BLOCKELEMENT', blockElement)
-      }
-    }, interval)
-  }
+function animateLoader(callback) {
+  var barElement = document.getElementById('bar')
+  var barSpeed = 1000
+  var barWidth = 0
+  var interval = setInterval(function() {
+    if (barWidth <= 100) {
+      barElement.style.width = barWidth + '%'
+      barWidth += 10
+    } else {
+      clearInterval(interval)
+      callback()
+    }
+  }, barSpeed)
 }
 
-// var MEETUP_API_URL = 'https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=CodeHub-Bristol&page=20'
-//
-// $.ajax({
-//   url: MEETUP_API_URL,
-//   type: "GET",
-//   dataType: 'jsonp',
-//   success: function (response) {
-//     console.log('RESPONSE', response)
-//     var js101Events = response.results.filter(function(event) {
-//       return event.name === 'JavaScript 101'
-//     })
-//     var nextEvent = js101Events.shift()
-//     console.log('NEXTEVENT', nextEvent)
-//     $('#about').append('<h1>'+nextEvent.name+'</h1>')
-//     $('#about').append('<p>Time: '+new Date(nextEvent.time)+'</p>')
-//     $('#about').append('<p>Venue: '+nextEvent.venue.name+', ' + nextEvent.venue.city + '</p>')
-//     $('#about').append(nextEvent.description)
-//     $('#about').append('<br><a href="'+nextEvent.event_url+'" target="_blank" id="next-event">Join the next event on Meet up</a>')
-//
-//     setInterval(function() {
-//       $('#next-event').toggleClass('hidden')
-//     }, 700)
-//   },
-//   error: function (xhr, status) {
-//     var eventUrl = 'https://www.meetup.com/CodeHub-Bristol/events/'
-//     $('#about').append('<br><br><a href="'+eventUrl+'" target="_blank">Join the next JavaScript101 event on Meetup</a>')
-//   }
-// });
+function iCanHazCheezburger() {
+  $('#root')
+    .empty()
+    .append('<marquee direction="down" width="100%" height="600px" behavior="alternate" style="border:solid"><marquee behavior="alternate"><img src="holding-page/img/cheezburger.jpg"></marquee></marquee>')
+}
+
+function bindMouseTrapEvents() {
+  Mousetrap.bind(['up down left right enter', 'j s'], function() {
+    iCanHazCheezburger();
+  });
+  Mousetrap.bind(['esc', 'space', 'enter'], function() {
+    renderNextEventPage();
+  });
+}
+
+function renderNextEventPage() {
+  var EVENT_NAME = 'JavaScript 101'
+  var NEXT_EVENT_URL = 'https://api.meetup.com/2/events?&sign=true&photo-host=public&group_urlname=CodeHub-Bristol&page=20'
+  var EVENT_CALENDAR_URL = 'https://www.meetup.com/CodeHub-Bristol/'
+  var SECRECT_URL_SHH_DONT_TELL_ANYONE = 'https://goo.gl/CxoGZk'
+  $.ajax({
+    url: NEXT_EVENT_URL,
+    type: "GET",
+    dataType: 'jsonp',
+    success: function (response) {
+      var js101Events = response.results.filter(function(event) {
+        return event.name === EVENT_NAME
+      })
+      var nextEvent = js101Events.shift()
+
+      $('#root')
+        .empty()
+        .append('<h1>'+nextEvent.name+'</h1>')
+        .append('<p>Time: '+new Date(nextEvent.time)+'</p>')
+        .append('<p>Venue: '+nextEvent.venue.name+', ' + nextEvent.venue.city + '</p>')
+        .append('<br>'+nextEvent.description)
+        .append('<br><a href="'+nextEvent.event_url+'" target="_blank" id="next-event">Join the next event on Meet up</a>')
+        .append('<br><br><a href="'+SECRECT_URL_SHH_DONT_TELL_ANYONE+'" target="_blank">The secret to Javascripts</a>')
+
+      bindMouseTrapEvents()
+    },
+    error: function (xhr, status) {
+      $('#root')
+        .empty()
+        .append('<h1>Error</h1>')
+        .append('<h2>Something went wrong!</h2>')
+        .append('<h2>Please try refreshing the page or try the meetup link below.</h2>')
+        .append('<br><br><a href="'+EVENT_CALENDAR_URL+'" target="_blank">Join the next JavaScript101 event on Meetup</a>')
+    }
+  });
+}
